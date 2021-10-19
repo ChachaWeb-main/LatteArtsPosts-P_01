467c12e798150e00e87a07808d351af841cc5993
EC2 Default User <ec2-user@ip-172-31-42-160.us-east-2.compute.internal>
2021-10-19 18:37:26 +0900

    ログインデザイン変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,75 +1,66 @@
 @extends('layouts.admin')
 
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('ログイン/Login') }}</div>
+        <div class="col-md-5">
+            <h3 class="mt-3 mb-3">ログイン</h3>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <hr>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-5 col-form-label text-md-right">{{ __('メールアドレス/E-Mail Address') }}</label>
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror samazon-login-input" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="メールアドレス/Mail address">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-5 col-form-label text-md-right">{{ __('パスワード/Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('ログイン状態保持/Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('ログイン/Login') }}
-                                </button>
-                                
-                                <a href="/register" class="btn btn-secondary my-2" >{{ __('利用登録/Register') }}</a><br>
-
-                                @if (Route::has('password.request'))
-                                    <a href="{{ route('password.request') }}">
-                                        {{ __('パスワードお忘れですか？/Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>メールアドレスが正しくない可能性があります。</strong>
+                    </span>
+                    @enderror
                 </div>
+
+                <div class="form-group">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror samazon-login-input" name="password" required autocomplete="current-password" placeholder="パスワード/Password">
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>パスワードが正しくない可能性があります。</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label samazon-check-label w-100" for="remember">
+                            ログイン状態保持/Remember Me
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="mt-3 btn samazon-submit-button w-100">
+                        ログイン/Login
+                    </button>
+
+                    <a class="btn btn-link mt-3 d-flex justify-content-center samazon-login-text" href="{{ route('password.request') }}">
+                        パスワードをお忘れですか?/Forgot Your Password?
+                    </a>
+                </div>
+            </form>
+
+            <hr>
+
+            <div class="form-group">
+                <a class="btn btn-link mt-3 d-flex justify-content-center samazon-login-text" href="{{ route('register') }}">
+                    新規登録/Register
+                </a>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
