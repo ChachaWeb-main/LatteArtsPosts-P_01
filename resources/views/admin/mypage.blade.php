8be0ff1ec0152c0d0aa2d56be16c5d69f63c3837
EC2 Default User <ec2-user@ip-172-31-42-160.us-east-2.compute.internal>
2021-10-31 17:36:47 +0900

    profile削除機能out

--- resources/views/admin/mypage.blade.php
+++ resources/views/admin/mypage.blade.php
@@ -88,22 +88,14 @@
         </tbody>
     </table>
     <div class="edit">
-        <a href = "{{action('Admin\ProfileController@edit', ['id'=>$logged_in_user->profile->id]) }}" class="btn btn-link" >編集/Edit</a>
-        <!--<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">-->
-        <!--    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>-->
-        <!--    <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>-->
-        <!--</svg>-->
+        <a href = "{{action('Admin\ProfileController@edit', ['id'=>$logged_in_user->profile->id]) }}" class="btn btn-link fs-5" >編集/Edit</a>
     </div>
-    <div class="delete">
+    {{--<div class="delete">
         <form method="post" action="{{action('Admin\ProfileController@delete', ['id'=>$logged_in_user->profile->id]) }}">
             @csrf
             <button type="button" class="btn btn-link">削除/Delete</button>
         </form>
-        <!--<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">-->
-        <!--    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>-->
-        <!--    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>-->
-        <!--</svg>-->
-    </div>
+    </div>--}}
     <br>
     <br>
     {{-- vies/admin/latte/index.blade.php--}}
@@ -158,18 +150,10 @@
                                     </div>
                                 </p>
                                 <a href = "{{action('Admin\LatteController@edit', ['id' => $latte -> id]) }}" class="btn btn-link">編集/Edit</a>
-                                    <!--<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">-->
-                                    <!--  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>-->
-                                    <!--  <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>-->
-                                    <!--</svg>-->
                                 <form method="post" action="{{ action('Admin\LatteController@delete', ['id' => $latte -> id]) }}" onSubmit="return check()">
                                     @csrf
                                     <button type="submit" class="btn btn-link">削除/Delete</button>
                                 </form>
-                                    <!--<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">-->
-                                    <!--  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>-->
-                                    <!--  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>-->
-                                    <!--</svg>-->
                             </div>
                         </div>
                     @endforeach
